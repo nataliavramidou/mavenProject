646a3612da5701cc25342f914f40145b684cf144
nataliavramidou <nata_av11@hotmail.com>
2019-05-08 12:28:48 +0300

    Unittesting module

---
+++ seip2019/unittesting/src/test/java/operations/MyIntegerOperationsTestSuite.java
@@ -0,0 +1,13 @@
+package operations;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({IntegerOperationsTest.class, IntegerOperationsTest_Parameterized.class})
+public class MyIntegerOperationsTestSuite {
+	/*
+	 * This class remains empty. It is used only as
+	 * a holder of the above annotations.
+	 */
+}
